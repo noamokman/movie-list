7fe0ae68c737dc0c28d7498a02f35ad405d8f8eb
Noam Okman <noamokman@gmail.com>
2016-04-06 23:32:16 +0300

    fix(tests): increase timeout for mockery

--- test/main.spec.js
+++ test/main.spec.js
@@ -123,7 +123,9 @@ describe('movie-list', () => {
   });
 
   describe('with valid options', () => {
-    beforeEach(() => {
+    beforeEach(function () {
+      this.timeout(5000);
+
       const globbyMock = sinon.stub().returns(Promise.resolve(['San.Andreas.2015.720p.WEBRIP.x264.AC3-EVE.mkv', 'Aloha.2015.1080p.BluRay.x264.DTS-WiKi.mkv', 'server.error.1993.720p.DVDRIP.x264.AC3-EVE.mkv', 'not.found.1993.720p.DVDRIP.x264.AC3-EVE.mkv']));
       const omdbMock = {
         get (name, cb) {
